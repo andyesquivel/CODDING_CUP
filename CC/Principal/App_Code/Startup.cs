669d506233f755bbd6f5e211b3a82b2fffaa7b68
andyesquivel <and1.itsur@gmail.com>
2018-05-17 20:36:31 -0500

    CREACIÓN DEL SITIO WEB

---
+++ CC/Principal/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Principal.Startup))]
+namespace Principal
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
